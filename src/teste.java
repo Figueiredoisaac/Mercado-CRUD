8dd1cd909e7964ea1238ffb100512411e4fda34e
Isaac Figueiredo <caasikh.dev@gmail.com>
2022-09-20 23:47:48 -0300

    Novos testes e inclusão da classe Cadastrar
    Inclusão da biblioteca java.util para utilização do Scanner para pegar o
    input dos usuários.

--- src/teste.java
+++ src/teste.java
@@ -1,7 +1,7 @@
 public class teste {
 
 	public static void main(String[] args) {
-		Produto arroz = new Produto(1, "Arroz-branco", 1.50);
+		Produto arroz = new Produto(1, "Arroz Branco", 1.50);
 		System.out.println(arroz); 
 		
 	}
